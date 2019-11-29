200fa2272a0e6db0bb996d54a4ccb18fc27c4f05
SafoSnep <45212701+SafoSnep@users.noreply.github.com>
2019-11-29 18:37:46 +0200

    Fix Package.swift

--- Package.swift
+++ Package.swift
@@ -1,5 +1,18 @@
 import PackageDescription
 
 let package = Package(
-    name: "Highlighter"
+	name: "Highlighter",
+	platforms: [
+		.iOS(.v9),
+	],
+	products: [
+		.library(name: "Highlighter", targets: ["Highlighter"]),
+	],
+	targets: [
+		.target(
+			name: "Highlighter",
+			dependencies: [],
+			path: "Highlighter"
+		),
+	]
 )
